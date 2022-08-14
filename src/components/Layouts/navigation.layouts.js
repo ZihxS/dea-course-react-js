d7434571954c643b328eee99c4e7ae4c278e877b
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 16:48:55 +0700

    Update

--- src/components/Layouts/navigation.layouts.js
+++ src/components/Layouts/navigation.layouts.js
@@ -4,12 +4,12 @@ const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">ALWAYS NGODING</Navbar.Brand>
+        <Navbar.Brand href="/">ALWAYS NGODING</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">IMAGES</Nav.Link>
-            <Nav.Link href="#link">POSTS</Nav.Link>
+            <Nav.Link href="/images">IMAGES</Nav.Link>
+            <Nav.Link href="/posts">POSTS</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
