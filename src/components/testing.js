0aee5c4831d7b3a5a6116586d65859e830355c4f
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 13:54:36 +0700

    Update

--- src/components/testing.js
+++ src/components/testing.js
@@ -4,11 +4,16 @@
 
 import React from "react"
 
+const x = (a, b) => {
+  return a + b
+}
+
 const Testing = (props) => {
   return (
     <React.Fragment>
       <h1>{props.title}</h1>
       <p>{props.description}</p>
+      {x(3, 5)}
     </React.Fragment>
   )
 }
