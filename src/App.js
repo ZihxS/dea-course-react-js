3629e8cee48fb91073d5a2135bc18eaae42f0127
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 14:20:40 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <>
       <MainLayouts>
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
