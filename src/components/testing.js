a299ff26d538b702cd48f547cd24e9321623348c
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 11:19:38 +0700

    Update

---
+++ src/components/testing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Testing = () => {
+  return (
+    <React.Fragment>
+      <h1>TESTING</h1>
+      <p>HELLO TESTING DESCRIPTION</p>
+    </React.Fragment>
+  )
+}
+
+export default Testing;
