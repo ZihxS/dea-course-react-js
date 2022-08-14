e5399eb1a83fdd0b40ff1b97a84703f0d35d95f3
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 16:48:28 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css'
 import React, { useEffect, useRef, useState } from 'react'
 import Albums from './components/Albums/main.albums'
 import MainLayouts from './components/Layouts/main.layouts'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom"
 
 const App = () => {
   return (
     <>
       <MainLayouts>
-        <Albums title="Image API" description="We fetch random image API from third party." />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Albums />} />
+            <Route path="/posts" element={<h1>POSTING</h1>} />
+          </Routes>
+        </Router>
+        {/* <Albums title="Image API" description="We fetch random image API from third party." /> */}
       </MainLayouts>
     </>
   )
