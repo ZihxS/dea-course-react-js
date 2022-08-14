2efb2f3ba5c45fad626c58c704ba2d0d811ebb6b
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 14:54:41 +0700

    Update

--- src/components/images.js
+++ src/components/images.js
@@ -3,12 +3,15 @@
 // kalo untuk penulisan comp itu bisa disebut PascalCase ya untuk var itu camelCase ya
 
 import React from "react"
+import { Container } from "react-bootstrap"
 
 const Images = (props) => {
   return (
     <React.Fragment>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <Container>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </Container>
     </React.Fragment>
   )
 }
