9cc1ec6265de17623c28c920fb8310364be9074a
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 16:09:43 +0700

    Update

--- src/components/Albums/collection.albums.js
+++ src/components/Albums/collection.albums.js
@@ -24,23 +24,19 @@ const Collection = () => {
         {datas.map((data, i) => {
           console.table(data)
           return (
-            <div key={i}>
-              <h1>{data.url}</h1>
-              <p>{data.title}</p>
-            </div>
+            <Carousel.Item key={i}>
+              <img
+                className="d-block w-100"
+                src={data.url}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>{data.title}</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item> 
           )
         })}
-        {/* <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/600/92c952"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item> */}
       </Carousel>
       {/* carousel items end */}
     </React.Fragment>
