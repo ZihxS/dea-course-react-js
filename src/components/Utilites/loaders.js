6c331e911432173e7b6986168d9c57e23db4a8eb
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 16:59:33 +0700

    Update

--- src/components/Utilites/loaders.js
+++ src/components/Utilites/loaders.js
@@ -2,10 +2,12 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loaders = () => {
   return (
-    <div>
-      <Spinner animation="grow" variant="primary" />
-      <Spinner animation="grow" variant="warning" />
-      <Spinner animation="grow" variant="success" />
+    <div className='d-flex justify-content-center align-items-center' style={{minHeight: "100vh"}}>
+      <div className='d-flex'>
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="warning" />
+        <Spinner animation="grow" variant="success" />
+      </div>
     </div>
   )
 }
