ebd34bd890015ab26fe5bdbc003bc9e812bb9bf2
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 12:10:08 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Testing from './components/testing'
 
 const App = () => {
   // kiri nilai, kanan buat set nilai nya, yang dalem kurung useState itu nilai default nya
-  const [getLimit, setLimit] = useState(3)
+  const [getLimit, setLimit] = useState(1)
 
   return (
     <>
       <Testing/>
       <center>
-        <button onClick={() => setLimit(getLimit + 1)}>NAEKIN LIMIT</button>
+        <button onClick={console.log("asdasdasdasd")}>NAEKIN LIMIT</button>
         <br/>
         {getLimit}
       </center>
