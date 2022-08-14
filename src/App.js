6a5465e3559639f0e7651b5f7c7dbdde0c4c93e2
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 11:05:49 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    console.log("render pertama...")
+  }
+
   render() {
     return (
        <h1>TESTING</h1>
