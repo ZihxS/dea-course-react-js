5f95b3cd3c4a58206baf4709ccb8c919ccfac329
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 11:36:05 +0700

    Update

--- src/components/testing.js
+++ src/components/testing.js
@@ -2,15 +2,14 @@
 
 // kalo untuk penulisan comp itu bisa disebut PascalCase ya untuk var itu camelCase ya
 
-import React from "react"
-import Testing2 from "./testing2"
+import { Fragment } from "react"
 
 const Testing = () => {
   return (
-    <React.Fragment>
+    <Fragment>
       <h1>TESTING</h1>
       <p>HELLO TESTING DESCRIPTION</p>
-    </React.Fragment>
+    </Fragment>
   )
 }
 
