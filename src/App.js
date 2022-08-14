a0172d379f92e5363cf25e008d124dd4a38a6200
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 11:49:36 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import React, { useState } from 'react'
 import Testing from './components/testing'
 
 const App = () => {
-  // kiri nilai, kanan buat set nilai nya
-  const [x, y] = useState()
+  // kiri nilai, kanan buat set nilai nya, yang dalem kurung useState itu nilai default nya
+  const [getLimit, setLimit] = useState()
 
   return (
     <>
