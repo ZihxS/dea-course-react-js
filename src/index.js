bb9c58dd73210841231641cb744ca5a4b8aeba6e
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 11:10:05 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+// Rekomendasi extensi 
+// 1.env
+// 2.highlight matching tag
+// 3.javascript (ES6) code snippets
+// 4.javascript and typescript nightly
+// 5.javascript snippet pack
+// 6.node.js module
+// 7.npm intellisense
+// 8.path intellisense
+// 9.prettify json
+// 10.react/redux/react-router snippets
+// 11.simple react snippets
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
