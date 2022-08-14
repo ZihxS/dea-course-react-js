7a082fea51456f891c175386f7d788125928621d
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 14:29:49 +0700

    Update

--- src/components/Layouts/main.layouts.js
+++ src/components/Layouts/main.layouts.js
@@ -1,8 +1,10 @@
 import Container from 'react-bootstrap/Container'
+import Navigation from './navigation.layouts'
 
 const MainLayouts = ({children}) => {
   return (
     <Container>
+      <Navigation/>
       {children}
     </Container>
   )
