835dd57c90e7c95badeda0e290b2f35142827ae4
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-14 15:42:07 +0700

    Update

--- src/components/Albums/main.albums.js
+++ src/components/Albums/main.albums.js
@@ -4,13 +4,13 @@
 
 import React from "react"
 import { Container } from "react-bootstrap"
+import Collection from "./collection.albums"
 
 const Albums = (props) => {
   return (
     <React.Fragment>
       <Container className="mt-2">
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+        <Collection />
       </Container>
     </React.Fragment>
   )
